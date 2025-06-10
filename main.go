1ed9bb06f149906fd688b6b1925bcdfd6342041e
rongfengliang <1141591465@qq.com>
2025-06-10 22:03:55 +0800

    add more demo

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	easytierffigo "github.com/rongfengliang/easytier-ffi-go/easytier"
 )
 
+var max_length = 10
+
 func main() {
 	data, err := os.ReadFile("app.yaml")
 	if err != nil {
@@ -28,6 +30,15 @@ func main() {
 	}
 	for {
 		fmt.Println("looping...")
-		time.Sleep(1 * time.Second)
+		time.Sleep(3 * time.Second)
+		// Prepare a slice to hold the results
+		infos := make([]easytierffigo.KeyValuePair, max_length)
+		ret := easytierffigo.CollectNetworkInfos(&infos[0], uintptr(max_length))
+		if ret > 0 {
+			// Process the collected network info
+			for i := 0; i < int(ret); i++ {
+				fmt.Printf("Network Info [%d]: %s = %s\n", i, easytierffigo.CStrToGoStr(infos[i].Key), easytierffigo.CStrToGoStr(infos[i].Value))
+			}
+		}
 	}
 }
